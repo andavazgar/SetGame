d3c93ee84f821af8200990f14a2381ad86d84c45
Andres Vazquez <andavazgar.dev@gmail.com>
2022-02-25 13:23:49 -0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Andres Vazquez on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
